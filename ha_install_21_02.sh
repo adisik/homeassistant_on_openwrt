c784a3573a94718bdbcd3c92a7fc0064900c01a3
adisik <fps@tut.by>
2021-05-22 21:40:30 +0300

    made changes

--- ha_install_21_02.sh
+++ ha_install_21_02.sh
@@ -179,8 +179,8 @@ protobuf==3.15.8
 beautifulsoup4==4.9.1
 
 # zha requirements
-pyserial==3.5
-zha-quirks==0.0.51
+# pyserial==3.5
+# zha-quirks==0.0.51
 # zigpy==0.30.0
 # zigpy-zigate==0.7.3
 EOF
